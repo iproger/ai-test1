9b0797c6f27bbb2bf924f69f424c3e5c6043b88c
Michael Shatilov <proger.mixa@gmail.com>
2025-06-20 17:25:12 -0400

    rebuild assets

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,15 @@
-export { default as amd } from './cpuModels/amd.json';
-export { default as apple } from './cpuModels/apple.json';
-export { default as intel } from './cpuModels/intel.json';
-export { default as snapdragon } from './cpuModels/snapdragon.json';
-export type CpuModel = typeof amd;
-export const models = { amd, apple, intel, snapdragon };
+import amdModel from './cpuModels/amd.json';
+import appleModel from './cpuModels/apple.json';
+import intelModel from './cpuModels/intel.json';
+import snapdragonModel from './cpuModels/snapdragon.json';
+
+export type CpuModel = typeof amdModel;
+
+export const models = {
+  amd: amdModel,
+  apple: appleModel,
+  intel: intelModel,
+  snapdragon: snapdragonModel,
+};
+
 export const modelList = Object.values(models);
